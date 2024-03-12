378f956fca5e2808e070478ecefff3343d218f68
Muhammad Howatab <Howdyhawt@gmail.com>
2024-03-12 14:48:32 +0500

    INITIAL COMMIT

---
+++ main.py
@@ -0,0 +1,15 @@
+import ttkbootstrap as ttk
+import random
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = ttk.Window(title="Password Manager")
+
+
+
+
+window.mainloop()
