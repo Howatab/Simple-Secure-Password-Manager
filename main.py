97b867f70d1c9165f833a698f936cb7738427966
Muhammad Howatab <Howdyhawt@gmail.com>
2024-03-13 19:39:15 +0500

    -m "Search Function and JSON data handling"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ttkbootstrap as ttk
 from tkinter import END,messagebox
 import random
-import pandas
+import json
 import string
 
 printables = list(string.ascii_letters+string.digits+ string.punctuation)
@@ -20,19 +20,39 @@ def add_button():
     email = EmailEntry.get()
     password = PasswordEntry.get()
     website = WebsiteEntry.get()
-    data = pandas.read_csv("Password_data.csv")
-    if messagebox.askyesno('Confirmation' ,message= f" Website : {website} \n Email = {email} \n Password: {password}\n Is being added to the Data file"):
-        new_row = {'website': website, 'email': email, 'password': password}
-        data = pandas.concat([data, pandas.DataFrame([new_row])], ignore_index=True)
-        data.to_csv("Password_data.csv",index=False)
+    new_data = {website :{
+        'email' : email,
+        'password' : password
+    }
+            }
+    if len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops" , message= "Empty Fields ")
+    elif messagebox.askyesno('Confirmation' ,message= f" Website : {website} \n Email = {email} \n Password: {password}\n Is being added to the Data file"):
+        with open("data.json" , 'r') as data_file:
+            
+            data = json.load(data_file)
+            data.update(new_data)
+            
+        with open("data.json" , 'w') as data_file:
+            json.dump(data,data_file,indent=4)
+            
         WebsiteEntry.delete(0,END)
         PasswordEntry.delete(0,END)
-    
+
+# ---------------------------- Search Function ------------------------------- #
+def search_button():
+    with open('data.json' , 'r') as data_file:
+        data_set = json.load(data_file)
+        search_target = WebsiteEntry.get()
+        if search_target in data_set:
+            email = data_set[search_target]['email']
+            password =  data_set[search_target]['password']
+            messagebox.showinfo(title="Results" , message=f"Website: {search_target}\nEmail: {email}\nPassword: {password}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = ttk.Window(title="Password Manager",themename='darkly')
-window.config(padx=50,pady=20)
+window.config(padx=100,pady=20)
 
 #Variables
 Password_var = ttk.StringVar(value="")
@@ -53,6 +73,12 @@ WebsiteEntry = ttk.Entry(width=45)
 WebsiteEntry.focus()
 WebsiteEntry.grid(column=1,row=1,columnspan=3,padx=20,pady=10 ,sticky='w')
 
+#Search Button
+AddButton = ttk.Button(text="Search" , width=8 ,command=search_button )
+AddButton.grid(column=1, row = 1 ,pady=20,sticky='e')
+
+
+
 #Email Entry
 
 
@@ -72,7 +98,7 @@ PasswordEntry = ttk.Entry(width=35,textvariable=Password_var)
 PasswordEntry.grid(column=1,row=3,padx=22,pady=10,sticky='w',columnspan=2)
 
 PasswordGen_button = ttk.Button(text="Generate",command=Password_generator)
-PasswordGen_button.grid(column=2, row = 3,sticky='e')
+PasswordGen_button.grid(column=1, row = 3,sticky='e')
 
 
 
